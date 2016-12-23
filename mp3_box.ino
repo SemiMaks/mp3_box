1151ff7be4d6c555dbe83c7b670b53dd82092398
SemiMaks <semimaks@mail.ru>
2016-12-24 00:44:42 +0300

    Правка кода для RC522

--- mp3_box.ino
+++ mp3_box.ino
@@ -1,6 +1,5 @@
-
-
-/* Typical pin layout used:
+/*
+ * Typical pin layout used:
  * -----------------------------------------------------------------------------------------
  *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
  *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
@@ -24,29 +23,38 @@
 #define RST_PIN 9
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);        // Create MFRC522 instance.
+MFRC522::MIFARE_Key key;
 
 void setup() {
         Serial.begin(9600);        // Initialize serial communications with the PC
         while (!Serial);           // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
         SPI.begin();               // Init SPI bus
         mfrc522.PCD_Init();        // Init MFRC522 card
-        Serial.println(F("Reed card: "));
+        Serial.println(F("Wait card: "));
         
-      }
+        // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+        for (byte i = 0; i < 6; i++) {
+                key.keyByte[i] = 0xFF;
+        }
+}
+
 void loop() {
   
         // Look for new cards, and select one if present
         if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
             delay(50);
             return;
-        }        
-       
+        }
+        
+        // Now a card is selected. The UID and SAK is in mfrc522.uid.
+        
+        // Dump UID
         Serial.print(F("Card UID:"));
         for (byte i = 0; i < mfrc522.uid.size; i++) {
                 Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
                 Serial.print(mfrc522.uid.uidByte[i], HEX);
         } 
         Serial.println();
-      
-
+       
+        delay(2000);
 }
