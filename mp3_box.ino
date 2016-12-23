1a0709f6de84697271b178b88668911209bc11de
SemiMaks <semimaks@mail.ru>
2016-12-24 02:06:46 +0300

    Добавление коментариев

--- mp3_box.ino
+++ mp3_box.ino
@@ -1,5 +1,5 @@
 /*
- * Typical pin layout used:
+ * Подключение пинов Arduino к RC522:
  * -----------------------------------------------------------------------------------------
  *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
  *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
@@ -10,29 +10,30 @@
  * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
- * The reader can be found on eBay for around 5 dollars. Search for "mf-rc522" on ebay.com. 
  */
 
 #include <SPI.h>
 #include <MFRC522.h>
 
-/* Set your new UID here! */
+/* Установка UIDa */
 #define NEW_UID {0xDE, 0xAD, 0xBE, 0xEF}
 #define SS_PIN 10
 #define RST_PIN 9
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);        // Create MFRC522 instance.
+const int LED = 13;
+const int CARD_DELAY = 500;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);        // Назначение пинов считывателя.
 MFRC522::MIFARE_Key key;
 
 void setup() {
-        Serial.begin(9600);        // Initialize serial communications with the PC
-        while (!Serial);           // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-        SPI.begin();               // Init SPI bus
-        mfrc522.PCD_Init();        // Init MFRC522 card
+        Serial.begin(9600);        // Инициализация последнего обмена данными с ПК
+        while (!Serial);           // Ожидание открытия порта
+        SPI.begin();               // Инициализация  шины
+        mfrc522.PCD_Init();        // Инициализация считывателя
         Serial.println(F("Wait card: "));
         
-        // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+        // Установка ключа в FFFFFFFFFFFFh заводского номера.
         for (byte i = 0; i < 6; i++) {
                 key.keyByte[i] = 0xFF;
         }
@@ -40,13 +41,13 @@ void setup() {
 
 void loop() {
   
-        // Look for new cards, and select one if present
+        // Просмотр карты, если приложена запомнить
         if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
             delay(50);
             return;
         }
         
-        // Now a card is selected. The UID and SAK is in mfrc522.uid.
+        // Выбор карты: UID
         
         // Dump UID
         Serial.print(F("Card UID:"));
@@ -56,5 +57,5 @@ void loop() {
         } 
         Serial.println();
        
-        delay(500);
+        delay(CARD_DELAY);
 }
