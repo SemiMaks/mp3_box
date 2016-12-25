fe1701e46c87ab6b22d2c31cc639310bb3602560
SemiMaks <semimaks@mail.ru>
2016-12-25 03:48:58 +0300

    Добавил библиотеку для модуля sd

--- mp3_box.ino
+++ mp3_box.ino
@@ -1,3 +1,5 @@
+#include <Wtv020sd16p.h>
+
 /*
  * Подключение пинов Arduino к RC522:
  * -----------------------------------------------------------------------------------------
@@ -14,57 +16,74 @@
 
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Wtv020sd16p.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
-#define LED 13
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);        // Назначение пинов считывателя.
 
-const int CARD_DELAY = 500;
+const int card_delay = 500;
+const int wait_card = 250;
 unsigned long uidDec, uidDecTemp;
 
+const unsigned int PLAY_PAUSE = 0xFFFE;
+const unsigned int STOP = 0xFFFF;
+const unsigned int VOLUME_MIN = 0xFFF0;
+const unsigned int VOLUME_MAX = 0xFFF7;
+
+int resetPin = 3;
+int clockPin = 4;
+int dataPin = 5;
+int busyPin = 6;
+int x = 0;
+
+Wtv020sd16p wtv020sd16p(resetPin, clockPin, dataPin, busyPin);
+
+
 void setup() {
+  
+        wtv020sd16p.reset();
+        
         Serial.begin(9600);        // Инициализация последнего обмена данными с ПК
         while (!Serial);           // Ожидание открытия порта
         SPI.begin();               // Инициализация  шины
         mfrc522.PCD_Init();        // Инициализация считывателя
-        Serial.println(F("Wait card: "));
-        
-        pinMode(LED, OUTPUT);
-
+        Serial.println(F("Wait card: "));      
 }
 
 void loop() {
+        delay(wait_card);
+       // wtv020sd16p.sendCommand(0xFFF6);
         // Просмотр карты, если приложена запомнить
         if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
             delay(50);
             return;
         }
+        
         uidDec = 0;
-        
-        // Выбор карты: UID
-        
+                
         for (byte i = 0; i < mfrc522.uid.size; i++)
          {
          uidDecTemp = mfrc522.uid.uidByte[i];
          uidDec = uidDec * 256 + uidDecTemp;
          }
-  Serial.println("Card UID: ");
-  Serial.println(uidDec); // Выводим UID метки в консоль.
+         Serial.println("Card UID: ");
+         Serial.println(uidDec); // Выводим UID метки в консоль.
   
-  if (uidDec == 429208694) // Сравниваем Uid метки, если он равен заданому - включаем музыку.
-  {
-    Serial.println("Super!");
-  }
-  else if (uidDec == 1100222070) // Сравниваем Uid метки, если он равен заданому - включаем музыку.
-  {
-    Serial.println("Neploho!");
-  }
-  else if (uidDec == 3239272822) // Сравниваем Uid метки, если он равен заданому - включаем музыку.
-  {
-    Serial.println("Otlichno!");
-  }
+         if (uidDec == 429208694) // Сравниваем Uid метки, если он равен заданому - включаем музыку.
+         {
+         Serial.println("Super!");
+         delay(wait_card);
+         }
+          else if (uidDec == 1100222070) // Сравниваем Uid метки, если он равен заданому - включаем музыку.
+         {
+         Serial.println("Neploho!");
+         }
+         else if (uidDec == 3239272822) // Сравниваем Uid метки, если он равен заданому - включаем музыку.
+         {
+         Serial.println("Otlichno!");
+         }
        
-    delay(CARD_DELAY);
+ delay(card_delay);
 }
